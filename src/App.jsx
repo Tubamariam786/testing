62a8247969d851179dd998f9d0c5b7ec09c8b0ac
tubamariamlaghari <tubamariam29@gmail.com>
2024-11-13 10:51:44 -0800

    remove comment

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,14 @@ import Checkbox from "./component/multiple/Checkbox";
 
 function App() {
   return (
-    // <div className="  h-screen">
-      <div className="w-full flex">
-        <div className="w-2/5 bg-gray-400 hidden md:display">
-          <SideBar />
-        </div>
-        <div className="w-3/5 bg-gray-700 mx-auto px-10">
-          <Checkbox />
-        </div>
+    <div className="w-full flex">
+      <div className="w-2/5 bg-gray-400 hidden md:display ">
+        <SideBar />
       </div>
-   
+      <div className="w-3/5 bg-gray-700 mx-auto px-10">
+        <Checkbox />
+      </div>
+    </div>
   );
 }
 
